d68a81c16f5e2ba430052e58da66563bac073c1d
Quang <quang.ha@rakuten.com>
2019-05-17 17:50:46 +0800

    add new target and re-import

---
+++ ObjectiveCpp-sample/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ObjectiveCpp-sample
+//
+//  Created by Ha, Quang on 2019/05/17.
+//  Copyright © 2019 Rakuten Asia Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
