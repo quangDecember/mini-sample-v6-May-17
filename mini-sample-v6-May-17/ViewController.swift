455997b3342019ec58aab5a8a92681e9d7e5b4d4
Quang <quang.ha@rakuten.com>
2019-05-17 16:59:18 +0800

    Initial Commit

---
+++ mini-sample-v6-May-17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mini-sample-v6-May-17
+//
+//  Created by Ha, Quang on 2019/05/17.
+//  Copyright © 2019 Rakuten Asia Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
